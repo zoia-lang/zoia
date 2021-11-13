9877d468e668708e7f8e8ca6bf3fa6f4f04a3458
Infernio <infernio@icloud.com>
2021-11-13 11:36:03 +0100

    Add initial grammar and scripts

---
+++ grammar/zoia.g4
@@ -0,0 +1,16 @@
+// Style guide:
+//  - Parser rules are camelCase
+//  - Lexer rules are PascalCase
+//    - Exception: fragments and skips (i.e. ones that users will
+//      _never_ see) are entirely uppercase.
+//  - Wrap to 70 characters
+//  - If a token has a single usage, move it up to its usage in the
+//    parser. Saves a line. Exception is if the definition is long or
+//    complex.
+
+grammar zoia;
+
+parseFile: EOF;
+
+// Ignore whitespace
+WHITESPACE: [ \t\n\r]+ -> skip;
---
+++ scripts/aliases.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Collection of useful functions and aliases when working on zoia.
+# Use via `source scripts/aliases.sh` from the top level.
+
+CLASSPATH=".:$(realpath scripts/antlr.jar)"
+export CLASSPATH
+
+ZOIA_TMP_DIR="$(realpath tmp/grammar)"
+
+# Parses a zoia file provided as the command line argument, regardless
+# of what your cwd is.
+function parse_zoia() {
+    test_file="$1"
+    if [ -z "$test_file" ]
+    then
+        echo "Missing argument: path to file"
+        return 1
+    fi
+
+    real_file=$(realpath "$test_file")
+    if [ ! -f "$real_file" ]
+    then
+        echo "$real_file does not exist or is not a file."
+        return 2
+    fi
+
+    cwd="$(pwd)"
+    if [ ! -d "$ZOIA_TMP_DIR" ]
+    then
+        echo "$ZOIA_TMP_DIR does not exist. Run scripts/build.sh to create it."
+        return 3
+    fi
+
+    cd "$ZOIA_TMP_DIR" || return
+    java org.antlr.v4.gui.TestRig zoia parseFile "$real_file" "${@:2}"
+    cd "$cwd" || return
+}
