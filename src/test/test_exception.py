acc44adfbb97f26d6b7431a40552dfd9b8ce315f
Infernio <infernio@icloud.com>
2021-12-02 21:44:59 +0100

    tests: Add tests for exception.py
    Brings us to 100% test coverage, but mostly I just want to see if
    coveralls will update correctly now.

---
+++ src/test/test_exception.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# GPL License and Copyright Notice ============================================
+#
+#   This file is part of Zoia, a language for writing fiction.
+#   Copyright (C) 2021 Infernio
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# =============================================================================
+"""This module houses tests related to custom exceptions."""
+from exception import AbstractError
+
+class TestAbstractError:
+    """AbstractError should correctly derive qualified method names."""
+    def test_abstract_msg(self):
+        try:
+            raise AbstractError(self.test_abstract_msg)
+        except AbstractError as e:
+            assert 'TestAbstractError.test_abstract_msg' in str(e)
